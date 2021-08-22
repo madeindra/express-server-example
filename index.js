02222070da31fab804abe180d92eaddea82561dd
Made Indra <made.indra@protonmail.com>
2021-08-22 17:35:02 +0700

    add register page

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
+// index.js
+
 const express = require('express'); // import module express
 const ejs = require('ejs'); // import module express
 const app = express(); // jalankan fungsi untuk inisiasi express
 
+const users = []; // siapkan array kosong untuk menampung data user
+
 const port = 3000; // port yang akan digunakan
 
-// fungsi ini nantinya akan digunakan saat alamat / diakses
-function index(req, res){
-  return res.render('index');
+app.use(express.urlencoded({ extended: false })); // middleware untuk membaca request body dari form
+app.set('view engine', 'ejs'); // middleware untuk me-render file ejs
+
+// fungsi ini nantinya akan digunakan saat mengkases alamat /
+function getUser(req, res){
+  res.json({
+    jumlahUser: users.length,
+    data: users 
+  });
 }
 
-// fungsi ini nantinya akan digunakan saat alamat /greet diakses
-function greet(req, res){
-  return res.render('greet', { name: req.query.name});
+// fungsi ini nantinya akan digunakan saat mengkases alamat /register dengan method GET
+function getRegister(req, res){
+  return res.render('register');
 }
 
-app.set('view engine', 'ejs');
+// fungsi ini nantinya akan digunakan saat mengakses alamat /register dengan method POST
+function postRegister(req, res){
+  // tambahan data ke array users
+  users.push({
+    email: req.body.email,
+    password: req.body.password
+  });
 
-// Ketika server diakses di alamat http://localhost:port/, jalankan fungsi helloWorld
-app.get('/', index);
+  // setelah itu, alihkan user ke alamat /
+  res.redirect('/');
+}
 
-// Ketika server diakses di alamat http://localhost:port/greet, jalankan fungsi greet, fungsi ini mendukung penambahan ?name=tulisNama
-app.get('/greet', greet);
+// Ketika mengakses alamat http://localhost:port/ dengan method GET, jalankan fungsi getUser
+app.get('/', getUser);
+
+// Ketika mengakses alamat http://localhost:port/register dengan method GET, jalankan fungsi getRegister
+app.get('/register', getRegister);
+
+// Ketika mengakses alamat http://localhost:port/register dengan method POST, jalankan fungsi postRegister
+app.post('/register', postRegister);
 
 // Ketika program dijalankan di port, jalankan fungsi onRun
 app.listen(port);
