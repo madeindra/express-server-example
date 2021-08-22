2687823da0fcf368a965f8b3e9ee331069e7f7da
Made Indra <made.indra@protonmail.com>
2021-08-22 18:18:14 +0700

    add statics and view

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require('express'); // import module express
+const ejs = require('ejs'); // import module ejs
 const app = express(); // jalankan fungsi untuk inisiasi express
 
 const port = 3000; // port yang akan digunakan
 
+app.set('view engine', 'ejs'); // gunakan ejs sebagai format untuk file tampilan (view)
+
+app.use(express.static('statics')); // gunakan middleware static untuk menyajikan file secara statik (tidak perlu membuat route), asset lokal tidak bisa digunakan tanpa ini
+
 // fungsi ini nantinya akan digunakan saat server diakses
-function helloWorld(req, res){
-  return res.send('Hello world');
+function homepage(req, res){
+  return res.render('index');
 }
 
-// fungsi ini nantinya akan digunakan saat program pertama kali dijalankan
-function onRun(){
-  console.log(`Server is running on http://localhost:${port}`);
-}
+// Ketika server diakses di alamat http://localhost:port/, jalankan fungsi homepage
+app.get('/', homepage);
 
-// Ketika server diakses di alamat http://localhost:port/, jalankan fungsi helloWorld
-app.get('/', helloWorld);
-
-// Ketika program dijalankan di port, jalankan fungsi onRun
-app.listen(port, onRun);
+// Jalankan server di port yang sudah ditentukan
+app.listen(port);
