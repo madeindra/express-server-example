31c750630ffe8c0fc8242bcd7d07ad8a1bd22289
Made Indra <made.indra@protonmail.com>
2021-08-22 18:33:10 +0700

    update response to sendFile

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express'); // import module express
-const ejs = require('ejs'); // import module ejs
+const path = require('path'); // import module path
+
 const app = express(); // jalankan fungsi untuk inisiasi express
 
 const port = 3000; // port yang akan digunakan
-
-app.set('view engine', 'ejs'); // gunakan ejs sebagai format untuk file tampilan (view)
 
 app.use(express.static('statics')); // gunakan middleware static untuk menyajikan file secara statik (tidak perlu membuat route), asset lokal tidak bisa digunakan tanpa ini
 
 // fungsi ini nantinya akan digunakan saat server diakses
 function homepage(req, res){
-  return res.render('index');
+  return res.sendFile(( path.join(__dirname, './views/index.html') ));
 }
 
 // Ketika server diakses di alamat http://localhost:port/, jalankan fungsi homepage
