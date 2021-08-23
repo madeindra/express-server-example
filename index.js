d37e71f62fed2ae4ba4b25d505883e4613dbd22d
Made Indra <made.indra@protonmail.com>
2021-08-23 18:10:29 +0700

    add sample method

--- index.js
+++ index.js
@@ -3,18 +3,43 @@ const app = express(); // jalankan fungsi untuk inisiasi express
 
 const port = 3000; // port yang akan digunakan
 
-// fungsi ini nantinya akan digunakan saat server diakses
-function helloWorld(req, res){
-  return res.send('Hello world');
-}
+app.use(express.json()); // menggunakan middleware json untuk membaca body berformat json
 
 // fungsi ini nantinya akan digunakan saat program pertama kali dijalankan
 function onRun(){
   console.log(`Server is running on http://localhost:${port}`);
 }
 
-// Ketika server diakses di alamat http://localhost:port/, jalankan fungsi helloWorld
-app.get('/', helloWorld);
+// handler untuk /user ketika menggunakan method GET
+function getUser(req, res){
+  res.status(200);
+  res.send('Anda mengakses /user dengan method GET');
+}
+
+// handler untuk /user ketika menggunakan method POST
+function postUser(req, res){
+  res.status(200);
+  res.send('Anda mengakses /user dengan method POST');
+}
+
+// handler untuk /user ketika menggunakan method PUT
+function putUser(req, res){
+  res.status(200);
+  res.send('Anda mengakses /user dengan method PUT');
+}
+
+// handler untuk /user ketika menggunakan method DELETE
+function deleteUser(req, res){
+  res.status(200);
+  res.send('Anda mengakses /user dengan method DELETE');
+}
+
+
+// route untuk user dengan 4 methods
+app.get('/user', getUser);
+app.post('/user', postUser);
+app.put('/user', putUser);
+app.delete('/user', deleteUser);
 
 // Ketika program dijalankan di port, jalankan fungsi onRun
 app.listen(port, onRun);
